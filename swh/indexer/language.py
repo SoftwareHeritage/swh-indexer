01493d01f762cd9452109172460dddfcda87439f
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-09-27 17:32:41 +0200

    Bayesian language programming detector approach

--- swh/indexer/language.py
+++ swh/indexer/language.py
@@ -5,34 +5,190 @@
 
 
 import click
+import json
+import os
+import math
+import sys
 
-from pygments.lexers import guess_lexer
-from pygments.util import ClassNotFound
 
-
-def run_language(path):
-    """Determine mime-type from file at path.
-
-    Args:
-        path: filepath to determine the mime type
-
-    Returns:
-        The possible language
+class LanguageDetector():
+    """Bayesian filter to learn and detect languages based on the source
+    code.
 
     """
+    def __init__(self):
+        self.data = {}
+        self.totals = {}
+
+    def tokenize(self, code):
+        """Split the source code in tokens.
+
+        Args:
+            code (str): source code
+
+        Yields:
+            list of words
+
+        """
+        for token in code.split():
+            if not token:
+                continue
+            yield token
+
+    def train(self, code, lang):
+        """Train to recognize the sample code as language lang.
+
+        Args:
+            code: the source code in language lang
+            lang: the language the source code is written in
+
+        """
+        lang_entry = {}
+        for word in self.tokenize(code):
+            lang_entry[word] = lang_entry.get(word, 0) + 1
+            self.totals[word] = self.totals.get(word, 0) + 1
+
+        self.data[lang] = lang_entry
+
+    def compute_prob(self, words, lang):
+        """Calculates the probability on the sample words for language lang.
+
+        Args:
+            words: word sample to compute probability on
+            lang: language concerned
+
+        Returns:
+            The probability for that sample in that language
+
+        """
+        res = 0.0
+        for word in words:
+            try:
+                res = res + math.log(self.totals[word]/self.data[lang][word])
+            except(KeyError):
+                continue
+        return res
+
+    def detect(self, code):
+        """Determine the most probable language for that source code.
+
+        This is completely dependent on the state of the classifier
+        once it has been trained (cf. `train` function)
+
+        Args:
+            code: the file's source code
+
+        Returns:
+            the most probable language for that source code
+
+        """
+        lang_prob = {}
+        words = list(self.tokenize(code))
+        for lang in self.data.keys():
+            prob = self.compute_prob(words, lang)
+            lang_prob[prob] = lang
+        return lang_prob[min(lang_prob.keys())]
+
+
+class JsonLanguageDetector(LanguageDetector):
+    """Language detector with load/dump json on disk abilities.
+
+    """
+    def dump(self, path):
+        """Dumps the detector's state into json at path.
+
+        Args:
+            path: Path to where dumping the json.
+
+        """
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, 'w') as f:
+            f.write(json.dumps({
+                'data': self.data,
+                'totals': self.totals
+            }))
+
+    def load(self, path):
+        """Load the detector's state from a json file.
+
+        Args:
+            path: Path to a json file. If the file does not exist, do nothing.
+
+        """
+        if not os.path.exists(path):
+            return
+
+        with open(path, 'r') as f:
+            data = json.loads(f.read())
+            if 'data'in data:
+                self.data = data['data']
+            if 'totals' in data:
+                self.totals = data['totals']
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command(help='Learn language from language snippet')
+@click.option('--path', required=1, help='Path to source code file to learn from')
+@click.option('--lang', required=1, help='Language for that source code')
+@click.option('--dump', help='Read and dump computed state at that path')
+def learn_one_language(path, lang, dump):
+    detector = JsonLanguageDetector()
+    if dump:
+        detector.load(path=dump)
+
+    with open(path, 'r') as f:
+        detector.train(f.read(), lang=lang)
+
+    if dump:
+        detector.dump(path=dump)
+
+
+@click.command(help='Learn language from language snippets')
+@click.option('--dump', help='Read and dump computed state at that path')
+def learn(dump):
+    detector = JsonLanguageDetector()
+
+    if dump:
+        detector.load(path=dump)
+
+    for path in sys.stdin:
+        path = path.rstrip()
+        name = os.path.splitext(path)[0]
+        lang = name.lower().replace('_', '-').replace(' ', '-')
+        print('From %s, learning %s' % (path, lang))
+        with open(path, 'r') as f:
+            detector.train(f.read(), lang=lang)
+
+    if dump:
+        detector.dump(path=dump)
+
+
+@click.command(help='Detect language from source code')
+@click.option('--path', required=1, help="Path to source code to detect language")
+@click.option('--dump', help='Read and dump computed state at that path')
+def detect(path, dump):
+    detector = JsonLanguageDetector()
+    if dump:
+        detector.load(path=dump)
+
     with open(path, 'r') as f:
         try:
-            return guess_lexer(f.read())
-        except ClassNotFound as e:
-            return None
+            lang = detector.detect(f.read())
+        except:
+            lang = None
+
+    print(lang)
+    return lang
 
 
-@click.command()
-@click.option('--path', help="Path to execute index on")
-def main(path):
-    r = run_language(path)
-    print(r)
+cli.add_command(learn_one_language)
+cli.add_command(learn)
+cli.add_command(detect)
 
 
 if __name__ == '__main__':
-    main()
+    cli()
