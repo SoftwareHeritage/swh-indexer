f996db7bdafd5724a14e4b9a4ce74be373187615
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-10-07 15:21:51 +0200

    Add ContentMimetypeIndexer
    - reading from objstorage
    - persist to storage
    
    Related T577

--- swh/indexer/file_properties.py
+++ swh/indexer/file_properties.py
@@ -6,6 +6,10 @@
 import click
 import subprocess
 
+from swh.core import hashutil
+
+from .indexer import BaseIndexer, DiskIndexer
+
 
 def run_file_properties(path):
     """Determine mimetype and encoding from file at path.
@@ -18,15 +22,79 @@ def run_file_properties(path):
 
     """
     cmd = ['file', '--mime-type', '--mime-encoding', path]
-    properties = subprocess.check_output(cmd, universal_newlines=True)
+    properties = subprocess.check_output(cmd)
     if properties:
-        res = properties.split(': ')[1].strip().split('; ')
+        res = properties.split(b': ')[1].strip().split(b'; ')
         mimetype = res[0]
-        encoding = res[1].split('=')[1]
+        encoding = res[1].split(b'=')[1]
         return {
             'mimetype': mimetype,
             'encoding': encoding
         }
+
+
+class ContentMimetypeIndexer(BaseIndexer, DiskIndexer):
+    """Indexer in charge of:
+    - filtering out content already indexed
+    - reading content from objstorage per the content's id (sha1)
+    - computing {mimetype, encoding} from that content
+    - store result in storage
+
+    """
+    ADDITIONAL_CONFIG = {
+        'workdir': ('str', '/tmp/swh/worker.file.properties'),
+    }
+
+    def __init__(self):
+        super().__init__()
+        self.working_directory = self.config['workdir']
+
+    def filter_contents(self, sha1s):
+        """Filter out known sha1s and return only missing ones.
+
+        """
+        yield from self.storage.content_mimetype_missing(sha1s)
+
+    def index_content(self, sha1, content):
+        """Index sha1s' content and store result.
+
+        Args:
+            sha1 (bytes): content's identifier
+            content (bytes): raw content in bytes
+
+        Returns:
+            A dict, representing a content_mimetype, with keys:
+              - id (bytes): content's identifier (sha1)
+              - mimetype (bytes): mimetype in bytes
+              - encoding (bytes): encoding in bytes
+
+        """
+        filename = hashutil.hash_to_hex(sha1)
+        content_path = self.write_to_temp(
+            filename=filename,
+            data=content)
+
+        properties = run_file_properties(content_path)
+        properties.update({
+            'id': sha1,
+        })
+
+        self.cleanup(content_path)
+        return properties
+
+    def persist_index_computations(self, results):
+        """Persist the results in storage.
+
+        Args:
+
+            results ([dict]): list of content_mimetype, dict with the
+            following keys:
+              - id (bytes): content's identifier (sha1)
+              - mimetype (bytes): mimetype in bytes
+              - encoding (bytes): encoding in bytes
+
+        """
+        self.storage.content_mimetype_add(results)
 
 
 @click.command()
--- swh/indexer/tasks.py
+++ swh/indexer/tasks.py
@@ -4,45 +4,14 @@
 # See top-level LICENSE file for more information
 
 from swh.scheduler.task import Task
-from .worker import ReaderWorker, FilePropertiesWorker, LanguageWorker
-from .worker import CtagsWorker
+from .file_properties import ContentMimetypeIndexer
 
 
-class SWHReaderTask(Task):
-    """Main task that read from storage the sha1's content.
-
-    """
-    task_queue = 'swh_indexer_worker_reader'
-
-    def run(self, *args, **kwargs):
-        ReaderWorker().run(*args, **kwargs)
-
-
-class SWHFilePropertiesTask(Task):
-    """Main task which computes the mime type from the sha1's content.
+class SWHContentMimetypeTask(Task):
+    """Main task which computes the mimetype, encoding from the sha1's content.
 
     """
-    task_queue = 'swh_indexer_worker_file_properties'
+    task_queue = 'swh_indexer_worker_content_mimetype'
 
     def run(self, *args, **kwargs):
-        FilePropertiesWorker().run(*args, **kwargs)
-
-
-class SWHLanguageTask(Task):
-    """Main task which computes the language from the sha1's content.
-
-    """
-    task_queue = 'swh_indexer_worker_language'
-
-    def run(self, *args, **kwargs):
-        LanguageWorker().run(*args, **kwargs)
-
-
-class SWHCtagsTask(Task):
-    """Main task which computes the ctags from the sha1's content.
-
-    """
-    task_queue = 'swh_indexer_worker_ctags'
-
-    def run(self, *args, **kwargs):
-        CtagsWorker().run(*args, **kwargs)
+        ContentMimetypeIndexer().run(*args, **kwargs)
