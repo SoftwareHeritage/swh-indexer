ebb5d332f3fad5795fce4cb651a97a3c97fc8b8f
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-10-12 11:41:43 +0200

    indexer: Add the check for presence as an indexer property

--- swh/indexer/orchestrator.py
+++ swh/indexer/orchestrator.py
@@ -22,6 +22,7 @@ class BaseOrchestratorIndexer(SWHConfig):
 
     DEFAULT_CONFIG = {
         'indexers': ('[str]', ['mimetype']),
+        'check_presence': ('bool', 'true'),
     }
 
     def __init__(self):
@@ -33,19 +34,37 @@ class BaseOrchestratorIndexer(SWHConfig):
             TASK_NAMES[name]: INDEXER_CLASSES[name]
             for name in indexer_names
         }
+        self.check_presence = self.config['check_presence']
 
-    def run(self, sha1s):
+    def run_with_check(self, sha1s):
+        """Run with checking the presence on sha1s in db.
+
+        """
         for task_name, indexer_class in self.indexers.items():
             indexer = indexer_class()
 
-            # first filter the contents per indexers
+            # filter the contents per indexers
             sha1s_filtered = list(indexer.filter_contents(sha1s))
 
             if not sha1s_filtered:
                 continue
 
-            # now send the message for the indexer to compute and store results
+            # send message for indexer to compute and store results
             app.tasks[task_name].delay(sha1s_filtered)
+
+    def run_no_check(self, sha1s):
+        """Simply broadcase sha1s to the indexers' queue.
+
+        """
+        for task_name, _ in self.indexers.items():
+            # send message for indexer to compute and store results
+            app.tasks[task_name].delay(sha1s)
+
+    def run(self, sha1s):
+        if self.check_presence:
+            self.run_with_check(sha1s)
+        else:
+            self.run_no_check(sha1s)
 
 
 class OrchestratorAllContentsIndexer(BaseOrchestratorIndexer):
