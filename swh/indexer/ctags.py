a4bc2108648bca9dfd9e5eebacdda227d1513db2
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-10-17 14:14:12 +0200

    Improve ctags run and parse results
    This uses universal-ctags's abilities to deal with more languages and to
    output json. Which simplifies the output parsing response.
    
    Related T589

--- swh/indexer/ctags.py
+++ swh/indexer/ctags.py
@@ -4,27 +4,20 @@
 # See top-level LICENSE file for more information
 
 import click
-import logging
-import os
 import subprocess
+import json
 
-from swh.core import utils
 
-# maximum number of detailed warnings for malformed tags that will be emitted.
-# used to avoid flooding logs
-BAD_TAGS_THRESHOLD = 5
-
-# Option used to generate the tag file
-CTAGS_FLAGS = [
-    '--excmd=number',
-    '--fields=+lnz',
-    '--sort=no',
-    '--links=no'
+# Options used to compute tags
+__FLAGS = [
+    '--fields=+lnz',  # +l: language of source file containing tag
+                      # +n: line number of tag definition
+                      # +z: include the symbol's kind (function, variable, ...)
+    '--sort=no',      # sort output on tag name
+    '--links=no',     # do not follow symlinks
+    '--output-format=json',  # outputs in json
 ]
 
-
-# debian: 'sudo update-alternatives --config ctags' and choose
-# ctags-exuberant
 
 def run_ctags(path, lang=None):
     """Run ctags on file path with optional language.
@@ -34,81 +27,29 @@ def run_ctags(path, lang=None):
         lang: language for that path (optional)
 
     Returns:
-        ctags' filepath
+        ctags' output
 
     """
-    ctagsfilepath = path + '.tags'
     optional = []
     # if lang:
     #     optional = ['--language-force', lang]
-    filename = os.path.basename(path)
-    cmd = ['ctags'] + CTAGS_FLAGS + optional + ['-o', ctagsfilepath, filename]
 
-    with utils.cwd(os.path.dirname(path)):
-        subprocess.check_call(cmd)
+    cmd = ['ctags'] + __FLAGS + optional + [path]
+    output = subprocess.check_output(cmd, universal_newlines=True)
 
-    return ctagsfilepath
-
-
-def parse_ctags(path):
-    """Parse exuberant ctags tags file.
-
-    Args:
-        path: Path to the ctag file
-
-    Yields:
-        For each tag, a tag dictionary with the keys:
-            - tag:  'TAG_NAME',
-            - path: 'PATH/WITH/IN/PACKAGE',
-            - line: LINE_NUMBER, # int
-            - kind: 'TAG_KIND', # 1 letter
-            - language: 'TAG_LANGUAGE',
-
-    """
-    def parse_tag(line):
-        tag = {'kind': None, 'line': None, 'language': None}
-        # initialize with extension fields which are not guaranteed to exist
-        fields = line.rstrip().split('\t')
-
-        tag['tag'] = fields[0]
-        tag['path'] = fields[1]
-
-        for ext in fields[3:]:  # parse extension fields
-            k, v = ext.split(':', 1)  # caution: "typeref:struct:__RAW_R_INFO"
-            if k == 'kind':
-                tag['kind'] = v
-            elif k == 'line':
-                tag['line'] = int(v)
-            elif k == 'language':
-                tag['language'] = v.lower()
-            else:
-                pass  # ignore other fields
-
-        assert tag['line'] is not None
-        return tag
-
-    with open(path) as ctags:
-        bad_tags = 0
-        for line in ctags:
-            # e.g. 'music\tsound.c\t13;"\tkind:v\tline:13\tlanguage:C\tfile:\n'
-            # see CTAGS(1), section "TAG FILE FORMAT"
-            if line.startswith('!_TAG'):  # skip ctags metadata
-                continue
-            try:
-                yield parse_tag(line)
-            except:
-                bad_tags += 1
-                if bad_tags <= BAD_TAGS_THRESHOLD:
-                    logging.warn('ignore malformed tag "%s"' % line.rstrip())
-        if bad_tags > BAD_TAGS_THRESHOLD:
-            logging.warn('%d extra malformed tag(s) ignored' %
-                         (bad_tags - BAD_TAGS_THRESHOLD))
+    for symbol in output.split('\n'):
+        if not symbol:
+            continue
+        js_symbol = json.loads(symbol)
+        yield {
+            k: v for k, v in js_symbol.items() if k != '_type' and k != 'path'
+        }
 
 
 @click.command()
 @click.option('--path', help="Path to execute index on")
 def main(path):
-    r = list(parse_ctags(path))
+    r = list(run_ctags(path))
     print(r)
 
 
