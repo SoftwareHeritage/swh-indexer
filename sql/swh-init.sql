515167591a16e02f2e9e81e26b3dc08daec31d65
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-03-09 10:42:27 +0100

    sql/swh-func: Move hash_sha1 function to the func namespace

--- sql/swh-init.sql
+++ sql/swh-init.sql
@@ -2,12 +2,3 @@ create extension if not exists btree_gist;
 create extension if not exists pgcrypto;
 
 create or replace language plpgsql;
-
-create or replace function hash_sha1(text)
-    returns text
-    language sql strict immutable
-as $$
-    select encode(digest($1, 'sha1'), 'hex')
-$$;
-
-comment on function hash_sha1(text) is 'Compute sha1 hash as text';
