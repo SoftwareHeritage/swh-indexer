a821a564da3cfca6f2f9bd2f55e667167fc5b1a2
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-03-09 10:41:21 +0100

    sql/swh-init.sql: Unify sql function like the rest

--- sql/swh-init.sql
+++ sql/swh-init.sql
@@ -4,9 +4,10 @@ create extension if not exists pgcrypto;
 create or replace language plpgsql;
 
 create or replace function hash_sha1(text)
-returns text
+    returns text
+    language sql strict immutable
 as $$
-select encode(digest($1, 'sha1'), 'hex')
-$$ language sql strict immutable;
+    select encode(digest($1, 'sha1'), 'hex')
+$$;
 
 comment on function hash_sha1(text) is 'Compute sha1 hash as text';
