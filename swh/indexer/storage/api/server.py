4b01a584a8cec433cebfd4a3957a5729ce247641
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-05-14 10:21:38 +0200

    storage.api.server: Only instantiate storage backend once per import
    Related 18c9dad986a1f6f19d57dd97079dc22ad10b04df

--- swh/indexer/storage/api/server.py
+++ swh/indexer/storage/api/server.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2015-2017  The Software Heritage developers
+# Copyright (C) 2015-2018  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
@@ -6,7 +6,7 @@
 import logging
 import click
 
-from flask import g, request
+from flask import request
 
 from swh.core import config
 from swh.core.api import (SWHServerAPIApp, decode_request,
@@ -27,6 +27,7 @@ DEFAULT_CONFIG = {
 
 
 app = SWHServerAPIApp(__name__)
+storage = None
 
 
 @app.errorhandler(Exception)
@@ -34,9 +35,12 @@ def my_error_handler(exception):
     return error_handler(exception, encode_data)
 
 
-@app.before_request
-def before_request():
-    g.storage = get_indexer_storage(**app.config[INDEXER_CFG_KEY])
+def get_storage():
+    global storage
+    if not storage:
+        storage = get_indexer_storage(**app.config[INDEXER_CFG_KEY])
+
+    return storage
 
 
 @app.route('/')
@@ -46,127 +50,127 @@ def index():
 
 @app.route('/check_config', methods=['POST'])
 def check_config():
-    return encode_data(g.storage.check_config(**decode_request(request)))
+    return encode_data(get_storage().check_config(**decode_request(request)))
 
 
 @app.route('/content_mimetype/add', methods=['POST'])
 def content_mimetype_add():
     return encode_data(
-        g.storage.content_mimetype_add(**decode_request(request)))
+        get_storage().content_mimetype_add(**decode_request(request)))
 
 
 @app.route('/content_mimetype/missing', methods=['POST'])
 def content_mimetype_missing():
     return encode_data(
-        g.storage.content_mimetype_missing(**decode_request(request)))
+        get_storage().content_mimetype_missing(**decode_request(request)))
 
 
 @app.route('/content_mimetype', methods=['POST'])
 def content_mimetype_get():
     return encode_data(
-        g.storage.content_mimetype_get(**decode_request(request)))
+        get_storage().content_mimetype_get(**decode_request(request)))
 
 
 @app.route('/content_language/add', methods=['POST'])
 def content_language_add():
     return encode_data(
-        g.storage.content_language_add(**decode_request(request)))
+        get_storage().content_language_add(**decode_request(request)))
 
 
 @app.route('/content_language/missing', methods=['POST'])
 def content_language_missing():
     return encode_data(
-        g.storage.content_language_missing(**decode_request(request)))
+        get_storage().content_language_missing(**decode_request(request)))
 
 
 @app.route('/content_language', methods=['POST'])
 def content_language_get():
     return encode_data(
-        g.storage.content_language_get(**decode_request(request)))
+        get_storage().content_language_get(**decode_request(request)))
 
 
 @app.route('/content/ctags/add', methods=['POST'])
 def content_ctags_add():
     return encode_data(
-        g.storage.content_ctags_add(**decode_request(request)))
+        get_storage().content_ctags_add(**decode_request(request)))
 
 
 @app.route('/content/ctags/search', methods=['POST'])
 def content_ctags_search():
     return encode_data(
-        g.storage.content_ctags_search(**decode_request(request)))
+        get_storage().content_ctags_search(**decode_request(request)))
 
 
 @app.route('/content/ctags/missing', methods=['POST'])
 def content_ctags_missing():
     return encode_data(
-        g.storage.content_ctags_missing(**decode_request(request)))
+        get_storage().content_ctags_missing(**decode_request(request)))
 
 
 @app.route('/content/ctags', methods=['POST'])
 def content_ctags_get():
     return encode_data(
-        g.storage.content_ctags_get(**decode_request(request)))
+        get_storage().content_ctags_get(**decode_request(request)))
 
 
 @app.route('/content/fossology_license/add', methods=['POST'])
 def content_fossology_license_add():
     return encode_data(
-        g.storage.content_fossology_license_add(**decode_request(request)))
+        get_storage().content_fossology_license_add(**decode_request(request)))
 
 
 @app.route('/content/fossology_license', methods=['POST'])
 def content_fossology_license_get():
     return encode_data(
-        g.storage.content_fossology_license_get(**decode_request(request)))
+        get_storage().content_fossology_license_get(**decode_request(request)))
 
 
 @app.route('/indexer_configuration/data', methods=['POST'])
 def indexer_configuration_get():
-    return encode_data(g.storage.indexer_configuration_get(
+    return encode_data(get_storage().indexer_configuration_get(
         **decode_request(request)))
 
 
 @app.route('/indexer_configuration/add', methods=['POST'])
 def indexer_configuration_add():
-    return encode_data(g.storage.indexer_configuration_add(
+    return encode_data(get_storage().indexer_configuration_add(
         **decode_request(request)))
 
 
 @app.route('/content_metadata/add', methods=['POST'])
 def content_metadata_add():
     return encode_data(
-        g.storage.content_metadata_add(**decode_request(request)))
+        get_storage().content_metadata_add(**decode_request(request)))
 
 
 @app.route('/content_metadata/missing', methods=['POST'])
 def content_metadata_missing():
     return encode_data(
-        g.storage.content_metadata_missing(**decode_request(request)))
+        get_storage().content_metadata_missing(**decode_request(request)))
 
 
 @app.route('/content_metadata', methods=['POST'])
 def content_metadata_get():
     return encode_data(
-        g.storage.content_metadata_get(**decode_request(request)))
+        get_storage().content_metadata_get(**decode_request(request)))
 
 
 @app.route('/revision_metadata/add', methods=['POST'])
 def revision_metadata_add():
     return encode_data(
-        g.storage.revision_metadata_add(**decode_request(request)))
+        get_storage().revision_metadata_add(**decode_request(request)))
 
 
 @app.route('/revision_metadata/missing', methods=['POST'])
 def revision_metadata_missing():
     return encode_data(
-        g.storage.revision_metadata_missing(**decode_request(request)))
+        get_storage().revision_metadata_missing(**decode_request(request)))
 
 
 @app.route('/revision_metadata', methods=['POST'])
 def revision_metadata_get():
     return encode_data(
-        g.storage.revision_metadata_get(**decode_request(request)))
+        get_storage().revision_metadata_get(**decode_request(request)))
 
 
 def run_from_webserver(environ, start_response,
