3cd387cce793c63121193b51cb0c89629e121a7d
Valentin Lorentz <vlorentz@softwareheritage.org>
2019-01-29 13:57:37 +0100

    Fix migration 119.

--- sql/upgrades/119.sql
+++ sql/upgrades/119.sql
@@ -17,3 +17,55 @@ alter table origin_intrinsic_metadata
 alter table origin_intrinsic_metadata
   alter column mappings
     drop default;
+
+
+create or replace function swh_revision_metadata_add(conflict_update boolean)
+    returns void
+    language plpgsql
+as $$
+begin
+    if conflict_update then
+      insert into revision_metadata (id, translated_metadata, mappings, indexer_configuration_id)
+      select id, translated_metadata, mappings, indexer_configuration_id
+    	from tmp_revision_metadata tcm
+            on conflict(id, indexer_configuration_id)
+                do update set translated_metadata = excluded.translated_metadata;
+
+    else
+        insert into revision_metadata (id, translated_metadata, mappings, indexer_configuration_id)
+        select id, translated_metadata, mappings, indexer_configuration_id
+    	from tmp_revision_metadata tcm
+            on conflict(id, indexer_configuration_id)
+            do nothing;
+    end if;
+    return;
+end
+$$;
+
+
+create or replace function swh_origin_intrinsic_metadata_add(
+        conflict_update boolean)
+    returns void
+    language plpgsql
+as $$
+begin
+    perform swh_origin_intrinsic_metadata_compute_tsvector();
+    if conflict_update then
+      insert into origin_intrinsic_metadata (origin_id, metadata, indexer_configuration_id, from_revision, metadata_tsvector, mappings)
+      select origin_id, metadata, indexer_configuration_id, from_revision,
+             metadata_tsvector, mappings
+    	from tmp_origin_intrinsic_metadata
+            on conflict(origin_id, indexer_configuration_id)
+                do update set metadata = excluded.metadata;
+
+    else
+        insert into origin_intrinsic_metadata (origin_id, metadata, indexer_configuration_id, from_revision, metadata_tsvector, mappings)
+        select origin_id, metadata, indexer_configuration_id, from_revision,
+               metadata_tsvector, mappings
+    	from tmp_origin_intrinsic_metadata
+            on conflict(origin_id, indexer_configuration_id)
+            do nothing;
+    end if;
+    return;
+end
+$$;
