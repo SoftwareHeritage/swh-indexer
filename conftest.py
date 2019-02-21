8de1d6a1b83891e248a65f585279b4d3a3f58678
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-02-21 14:42:27 +0100

    tests: Ignore wsgi module --doctest-modules forces module load
    This one cannot work because of the configuration loading it triggers.
    The configuration loading is not expected to be called at test time.

--- conftest.py
+++ conftest.py
@@ -4,3 +4,8 @@ from hypothesis import settings
 # https://hypothesis.readthedocs.io/en/latest/settings.html#settings-profiles
 settings.register_profile("fast", max_examples=5, deadline=5000)
 settings.register_profile("slow", max_examples=20, deadline=5000)
+
+# Ignore the following modules because wsgi module fails as no
+# configuration file is found (--doctest-modules forces the module
+# loading)
+collect_ignore = ['swh/indexer/storage/api/wsgi.py']
