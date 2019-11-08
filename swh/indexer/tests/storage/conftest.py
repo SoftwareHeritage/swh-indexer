01f1c385d0a1e3b5f97b34f57ed78f4f21b4f334
David Douard <david.douard@sdfa3.org>
2019-11-08 14:12:39 +0100

    tests: update/add copyright/license headers

--- swh/indexer/tests/storage/conftest.py
+++ swh/indexer/tests/storage/conftest.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2015-2019  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
 from os.path import join
 import pytest
 
