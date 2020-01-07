04f9cd13d3b265ab779fdcb80a8f1de18696d144
Valentin Lorentz <vlorentz@softwareheritage.org>
2020-01-07 17:05:14 +0100

    Fix type of 'author' in gemspec mapping output.
    It must be a Person (or Organization), not Text.

--- swh/indexer/tests/test_metadata.py
+++ swh/indexer/tests/test_metadata.py
@@ -213,7 +213,10 @@ class Metadata(unittest.TestCase):
             '@context': 'https://doi.org/10.5063/schema/codemeta-2.0',
             'name': 'test_metadata',
             'version': '0.0.2',
-            'author': 'moranegg',
+            'author': {
+                'type': 'Person',
+                'name': 'moranegg',
+            },
         }]
 
         # when
@@ -225,7 +228,10 @@ class Metadata(unittest.TestCase):
             "version": '0.0.2',
             "description": 'Simple package.json test for indexer',
             "name": ['test_1', 'test_0_1', 'test_metadata'],
-            "author": ['moranegg'],
+            "author": [{
+                'type': 'Person',
+                'name': 'moranegg'
+            }],
             "codeRepository":
                 'git+https://github.com/moranegg/metadata_test',
         }
@@ -1009,7 +1015,12 @@ end"""
         self.assertEqual(result, {
             '@context': 'https://doi.org/10.5063/schema/codemeta-2.0',
             'type': 'SoftwareSourceCode',
-            'author': ['Ruby Coder'],
+            'author': [
+                {
+                    'type': 'Person',
+                    'name': 'Ruby Coder'
+                }
+            ],
             'name': 'example',
             'license': 'https://spdx.org/licenses/MIT',
             'codeRepository': 'https://rubygems.org/gems/example',
@@ -1025,7 +1036,15 @@ Gem::Specification.new do |s|
 end"""
         result = self.gemspec_mapping.translate(raw_content)
         self.assertCountEqual(result.pop('author'), [
-            'Ruby Coder1', 'Ruby Coder2'])
+            {
+                'type': 'Person',
+                'name': 'Ruby Coder1'
+            },
+            {
+                'type': 'Person',
+                'name': 'Ruby Coder2'
+            },
+        ])
         self.assertEqual(result, {
             '@context': 'https://doi.org/10.5063/schema/codemeta-2.0',
             'type': 'SoftwareSourceCode',
@@ -1058,7 +1077,12 @@ end"""
         self.assertEqual(result, {
             '@context': 'https://doi.org/10.5063/schema/codemeta-2.0',
             'type': 'SoftwareSourceCode',
-            'author': ['Ruby Coder1'],
+            'author': [
+                {
+                    'type': 'Person',
+                    'name': 'Ruby Coder1'
+                }
+            ],
         })
 
     def test_gemspec_alternative_header(self):
