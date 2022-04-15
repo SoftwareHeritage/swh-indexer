270a2b3c74f4f66b9368bb21df84acf9c2287eba
Satvik Vemuganti <vemugantisesha@iitbhilai.ac.in>
2022-04-15 14:32:05 +0530

    Add annotations to the__init__.py, base.py, cff.py, codemeta.py and maven.py in metadata_dictionary

--- swh/indexer/metadata_dictionary/cff.py
+++ swh/indexer/metadata_dictionary/cff.py
@@ -1,3 +1,5 @@
+from typing import Dict, List, Optional, Union
+
 import yaml
 
 from swh.indexer.codemeta import CODEMETA_CONTEXT_URL, CROSSWALK_TABLE, SCHEMA_URI
@@ -18,19 +20,21 @@ class CffMapping(DictMapping, SingleFileMapping):
     mapping = CROSSWALK_TABLE["Citation File Format Core (CFF-Core) 1.0.2"]
     string_fields = ["keywords", "license", "abstract", "version", "doi"]
 
-    def translate(self, raw_content):
-        raw_content = raw_content.decode()
-        content_dict = yaml.load(raw_content, Loader=yaml.SafeLoader)
+    def translate(self, raw_content: bytes) -> Dict[str, str]:
+        raw_content_string: str = raw_content.decode()
+        content_dict = yaml.load(raw_content_string, Loader=yaml.SafeLoader)
         metadata = self._translate_dict(content_dict)
 
         metadata["@context"] = CODEMETA_CONTEXT_URL
 
         return metadata
 
-    def normalize_authors(self, d):
+    def normalize_authors(self, d: List[dict]) -> Dict[str, list]:
         result = []
         for author in d:
-            author_data = {"@type": SCHEMA_URI + "Person"}
+            author_data: Dict[str, Optional[Union[str, Dict]]] = {
+                "@type": SCHEMA_URI + "Person"
+            }
             if "orcid" in author:
                 author_data["@id"] = author["orcid"]
             if "affiliation" in author:
@@ -45,21 +49,21 @@ class CffMapping(DictMapping, SingleFileMapping):
 
             result.append(author_data)
 
-        result = {"@list": result}
-        return result
+        result_final = {"@list": result}
+        return result_final
 
-    def normalize_doi(self, s):
+    def normalize_doi(self, s: str) -> Dict[str, str]:
         if isinstance(s, str):
             return {"@id": "https://doi.org/" + s}
 
-    def normalize_license(self, s):
+    def normalize_license(self, s: str) -> Dict[str, str]:
         if isinstance(s, str):
             return {"@id": "https://spdx.org/licenses/" + s}
 
-    def normalize_repository_code(self, s):
+    def normalize_repository_code(self, s: str) -> Dict[str, str]:
         if isinstance(s, str):
             return {"@id": s}
 
-    def normalize_date_released(self, s):
+    def normalize_date_released(self, s: str) -> Dict[str, str]:
         if isinstance(s, str):
             return {"@value": s, "@type": SCHEMA_URI + "Date"}
